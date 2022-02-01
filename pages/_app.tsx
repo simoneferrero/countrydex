d9c2c4f5772075087b4cf09108aceaacdf5dec8e
simoneferrero <simone.ferrero@outlook.com>
2022-02-01 14:46:16 +0000

    First commmit

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import { UserProvider } from "@auth0/nextjs-auth0";
+
+import "@/styles/global.css";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
