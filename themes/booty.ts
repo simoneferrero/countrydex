a9998ef47f762fad0087093dc0e82aee50cdb191
simoneferrero <simone.ferrero@outlook.com>
2022-02-07 17:36:45 +0000

    Modified booty theme

--- themes/booty.ts
+++ themes/booty.ts
@@ -2,10 +2,10 @@ import type { Theme } from "types/Theme";
 
 const bootyTheme: Theme = {
   colors: {
-    "very-light": "#e4e5e6",
-    light: "#36454f",
-    medium: "#2b373f",
-    dark: "#161c20",
+    "very-light": "#e0c8d8",
+    light: "#db008f",
+    medium: "#8f005d",
+    dark: "#5c003c",
     "very-dark": "#0b0e10",
     single: "#dfb667",
     double: "#a0a0a0",
